cb42c402ccde93eedc57bb0d4e73bec2f626a5f0
LuoBin <2646887443@qq.com>
2025-03-31 20:40:27 +0800

    tuple_copy

---
+++ ch1/tuple_copy.py
@@ -0,0 +1,9 @@
+t1 = (1, 2, 3)
+t2 = tuple(t1)
+print(f't1 == t2: {t1 == t2}')
+print(f't1 is t2: {t1 is t2}')
+
+l1 = [1, 2, 3]
+l2 = list(l1)
+print(f'l1 == l2: {l1 == l2}')
+print(f'l1 is l2: {l1 is l2}')
