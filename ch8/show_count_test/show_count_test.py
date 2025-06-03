57b1163a98523eb9a5be5e789d031755c4394d27
LuoBin <2646887443@qq.com>
2025-06-03 11:55:40 +0800

    old version for mypy

--- ch8/show_count_test/show_count_test.py
+++ ch8/show_count_test/show_count_test.py
@@ -1,4 +1,6 @@
-def show_count(count: int, singular: str, plural: str | None = None) -> str:
+from typing import Optional
+
+def show_count(count: int, singular: str, plural: Optional[str] = None) -> str:
     if count == 1:
         return f'1 {singular}'
     count_str = str(count) if count else 'no'
