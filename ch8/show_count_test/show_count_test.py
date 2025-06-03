c860ff2659508cd32f5df6017b2b331da8a71d33
LuoBin <2646887443@qq.com>
2025-06-03 11:57:14 +0800

    use Union

--- ch8/show_count_test/show_count_test.py
+++ ch8/show_count_test/show_count_test.py
@@ -1,6 +1,6 @@
-from typing import Optional
+from typing import Optional, Union
 
-def show_count(count: int, singular: str, plural: Optional[str] = None) -> str:
+def show_count(count: int, singular: str, plural: Union[str, None] = None) -> str:
     if count == 1:
         return f'1 {singular}'
     count_str = str(count) if count else 'no'
